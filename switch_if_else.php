6a7cde13ae6d49bf726159ff8ccfe5eaff98f86e
kanon-X <syedur.b@uttarauniversity.edu.bd>
2025-11-25 15:14:56 +0600

    initial commit

---
+++ switch_if_else.php
@@ -0,0 +1,19 @@
+<?php
+$date = date("1");
+$date = "Wednesday";
+switch($date){
+    case "Monday":
+        echo "I hate Monday";
+        break;
+    case "Tuesday":
+        echo "It is Taco Tuesday";
+        break;
+    case "Wednesday":
+        echo "The work week is half over";
+        break;
+    case "Thursday":
+        echo "Its allmost the weekend";
+        break;
+
+
+}
